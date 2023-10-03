aae61dd777577262cf9e17c9d0e55517eb93ef9f
Abhishek Singh <mukundsinghg@gmail.com>
2023-10-03 21:57:16 +0530

    memoization added

---
+++ My topics/memoization.js
@@ -0,0 +1,43 @@
+//stores result in cache object so that function ko baar baar execute na krna pde
+const fn = (num)=>{
+    let res=0;
+    for(let i =1;i<=num;i++){
+        res+=i;
+    }
+    return res;
+}
+
+const memo = (fn)=>{
+    let cache={}
+    return function(...args){
+        let n =args[0];
+        if(n in cache){
+            console.log("from cache");
+            return cache[n];
+        }else{
+            cache[n]=fn(n);
+            console.log("NOT from cache");
+            return cache[n];
+        }
+    }
+}
+
+const efficient = memo(fn);
+
+console.time();
+console.log(efficient(10))
+console.timeEnd();
+
+console.time();
+console.log(efficient(10))
+console.timeEnd();
+console.time();
+console.log(efficient(10))
+console.timeEnd();
+
+console.time()
+console.log(efficient(15))
+console.timeEnd()
+console.time()
+console.log(efficient(2))
+console.timeEnd()
